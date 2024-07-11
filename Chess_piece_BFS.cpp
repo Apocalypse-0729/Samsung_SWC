8850cea8f3b5a09c3d32a0c2c3ce8c0d29f2e144
Hardik <101357543+Apocalypse-0729@users.noreply.github.com>
2024-07-11 17:45:09 +0530

    Create Chess_piece_BFS.cpp

---
+++ Chess_piece_BFS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int chess_piece(){
+    int n,m; cin>>n>>m;
+    int sr,sc,dr,dc;
+    cin>>sr>>sc>>dr>>dc;
+    vector<vector<int>> vis(n+1,vector<int>(m+1,0));
+    queue<pair<int,pair<int,int>>> q;
+    int ans=-1;
+    int move[8][2] = {{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};
+    q.push({0,{sr,sc}});
+    while(!q.empty()){
+        int row=q.front().second.first;
+        int col=q.front().second.second;
+        int steps=q.front().first;
+        q.pop();
+        for(int i=0; i<8; i++){
+            int nrow=row+move[i][0];
+            int ncol=col+move[i][1];
+            if(nrow==dr && ncol==dc) return steps+1;
+            if(nrow>=1 && nrow<=n && ncol>=1 && ncol<=m && !vis[nrow][ncol]){
+                vis[nrow][ncol]=1;
+                q.push({steps+1,{nrow,ncol}});
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        cout<<chess_piece()<<endl;
+    }
+    return 0;
+}
