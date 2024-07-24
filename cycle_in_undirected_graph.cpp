702f8c4652e51cf0f3bd960b6379f30674d5154f
Hardik <101357543+Apocalypse-0729@users.noreply.github.com>
2024-07-24 22:09:27 +0530

    Update cycle_in_undirected_graph.cpp

--- cycle_in_undirected_graph.cpp
+++ cycle_in_undirected_graph.cpp
@@ -1,24 +1,29 @@
-class Solution {
-public:
-    bool check(int node,vector<vector<int>> &graph, vector<int> &color){
-        for(auto it:graph[node]){
-            if(color[it]==-1){
-                color[it]=!color[node];
-                if(check(it,graph,color)==false) return false;
-            }
-            else if(color[it]==color[node]) return false;
-        }
-        return true;
-    }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int> color(n,-1);
-        for(int i=0; i<n; i++){
-            if(color[i]==-1){
-                color[i]=0;
-                if(check(i,graph,color)==false) return false; 
+bool bfsCheck(int n,int parent,vector<int> adj[],vector<int> &vis){
+        vis[n]=1;
+        queue<pair<int,int>> q;
+        q.push({n,parent});
+        while(!q.empty()){
+            int node=q.front().first;
+            int parentNode=q.front().second;
+            q.pop();
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push({it,node});
+                }
+                else if(it!=parentNode){
+                    return true;
+                }
             }
         }
-        return true;
+        return false;
     }
-};
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> vis(V,0);
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(bfsCheck(i,-1,adj,vis)==true) return true;
+            }
+        }
+        return false;
