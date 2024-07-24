3ceeb995f87129148f01c178e2bb182452a1cde6
Hardik <101357543+Apocalypse-0729@users.noreply.github.com>
2024-07-24 22:07:03 +0530

    Update cycle_in_directed_graph.cpp

--- cycle_in_directed_graph.cpp
+++ cycle_in_directed_graph.cpp
@@ -1,24 +1,24 @@
-class Solution {
-public:
-    bool check(int node,vector<vector<int>> &graph, vector<int> &color){
-        for(auto it:graph[node]){
-            if(color[it]==-1){
-                color[it]=!color[node];
-                if(check(it,graph,color)==false) return false;
+// Function to detect cycle in a directed graph.
+    bool dfsCheck(int node,vector<int> adj[],vector<int> &vis,vector<int> &pathVis){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfsCheck(it,adj,vis,pathVis)) return true;
             }
-            else if(color[it]==color[node]) return false;
+            else if(pathVis[it]) return true;
         }
-        return true;
+        pathVis[node]=0;
+        return false;
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int> color(n,-1);
+    bool isCyclic(int n, vector<int> adj[]) {
+        // code here
+        vector<int> vis(n,0);
+        vector<int> pathVis(n,0);
         for(int i=0; i<n; i++){
-            if(color[i]==-1){
-                color[i]=0;
-                if(check(i,graph,color)==false) return false; 
+            if(!vis[i]){
+                if(dfsCheck(i,adj,vis,pathVis)) return true;
             }
         }
-        return true;
+        return false;
     }
-};
